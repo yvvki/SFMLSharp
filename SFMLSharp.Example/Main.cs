69426f1087d9827cf1fda45ddbe3c96a613afd57
Yuuki Rika <yuuki.rikachan@gmail.com>
2022-01-24 16:15:42 +0700

    Updated example Main.

---
+++ SFMLSharp.Example/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Versioning;
+
+using SFML.Graphics;
+using SFML.Window;
+
+[assembly: RequiresPreviewFeatures]
+
+RenderWindow window = new();
+
+EventWindow eventWindow = new(window);
+eventWindow.Closed += () => window.Close();
+
+RectangleShape shape = new();
+
+while (window.IsOpen)
+{
+	window.Clear();
+
+	window.Draw(shape);
+}
